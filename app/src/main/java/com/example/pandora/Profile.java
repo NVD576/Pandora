a600f2b4bd4ca5abc3e00c7b406330b4885e11aa
Cyter <tu2004latoi@gmail.com>
2024-11-20 20:12:31 +0700

    optimize fragment

--- app/src/main/java/com/example/pandora/Profile.java
+++ app/src/main/java/com/example/pandora/Profile.java
@@ -1,5 +1,6 @@
 package com.example.pandora;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.pandora.Login.Login;
+import com.example.pandora.Main.Lobby;
 
 public class Profile extends Fragment {
 
@@ -18,7 +23,17 @@ public class Profile extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        TextView login = view.findViewById(R.id.loginProfile);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(getActivity(), Login.class);
+                startActivity(myIntent);
+            }
+        });
+
+        return view;
     }
 }
